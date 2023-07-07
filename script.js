332e5766680e0aafb4521136a11ed427fd1df252
cris90 <cristiantorrico90@gmail.com>
2023-07-07 06:54:13 -0300

    agregue los inconos y le dimos animacion con sus respectivas opciones

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
+// Obtener todos los elementos con la clase "foto-miembro"
+var icons = document.getElementsByClassName('foto-miembro');
 
+// Iterar sobre los elementos y agregar un evento de clic a cada uno
+for (var i = 0; i < icons.length; i++) {
+  icons[i].addEventListener('click', function() {
+    // Obtener el elemento de opciones correspondiente
+    var options = this.nextElementSibling;
 
+    // Alternar la clase "visible" para aplicar el efecto de desplazamiento suave
+    options.classList.toggle('visible');
+  });
+}
